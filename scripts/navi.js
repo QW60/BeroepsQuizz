6c2511f54f6486bc0676f55418b97d75c3381789
Quentin <104700929+QW60@users.noreply.github.com>
2024-03-21 10:07:49 +0100

    vfd
    vf

---
+++ scripts/navi.js
@@ -0,0 +1,11 @@
+function goto1() {
+    window.location.href = "./quiz/foodaroundtheworld";
+}
+
+function goto2() {
+    window.location.href = "./quiz/guessthewrongfood";
+}
+
+function goto3() {
+    window.location.href = "./quiz/pizza";
+}
